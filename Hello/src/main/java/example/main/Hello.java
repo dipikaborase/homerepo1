d2a5ea46ffda570ba38b10c45adc2a07a453675b
Dipika <dipikaborase07@gmail.com>
2022-12-23 18:19:40 +0530

    commit

---
+++ Hello/src/main/java/example/main/Hello.java
@@ -0,0 +1,11 @@
+package example.main;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Java");
+
+	}
+
+}
